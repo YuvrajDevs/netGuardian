120ee0a0b55d9a9be7d4feba3def4e7d227989ee
Sidhant Mishra <mishrasidhant01@gmail.com>
2024-02-23 10:45:55 +0530

    Fixed Drag and Drop

--- client/src/app/upload/page.jsx
+++ client/src/app/upload/page.jsx
@@ -1,9 +1,10 @@
+import DragNDrop from "../components/DragNDrop"
 
 
 const Upload = () => {
   return (
     <div>
-
+      <DragNDrop className={`mx-5 flex flex-col items-center justify-center h-[500px] bg-[#e1f4ff] border-[5px] border-solid border-[#dde4ff] rounded-xl mt-8`}/>
     </div>
   )
 }
